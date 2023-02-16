9ccb4fff115d26ffcf97d8dfdd292e281d163d9d
Biset Desalegn <bisetsamara@gmail.com>
2023-02-16 07:05:48 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/*including function */
+#include <stdio.h>
+
+/* Main function start here */
+int main () {
+   /*Printing the out put */ 
+   puts("\"Programming is like building a multilingual puzzle,");
+   return(0);  /*Return to 0*/
+   }
