2faacdfebfd2b9bdf6672eac28bda77b26db5c73
Biset Desalegn <bisetsamara@gmail.com>
2023-02-16 22:28:28 -0800

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 	float b;
 	printf("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(a));
 	printf("Size of an int :%lu byte(s)\n",(unsigned long)sizeof(x));
-	printf("Size of a long int %lu byte(s)\n",(unsigned long)sizeof(y);
-	printf("Size of a long long int:%lu byte\n",(unsigned long)sizeof(z);
+	printf("Size of a long int %lu byte(s)\n",(unsigned long)sizeof(y));
+	printf("Size of a long long int:%lu byte\n",(unsigned long)sizeof(z));
 	printf("Size of a float:%1ubyte(s)\n",(unsigned long)sizeof(b));
 	return(0);
 }
