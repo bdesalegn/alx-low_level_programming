83fa2b785cba0072fba0679dd27999ebfa4cfa06
Biset Desalegn <bisetsamara@gmail.com>
2023-02-17 14:41:43 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,9 +13,9 @@ int main(void)
 	/* prints all possible combinations of single-digit numbers. */
 	for (num = 0; num < 8; num++)
 	{
-		for (num1 = 0; num1 < 9; num1++)
+		for (num1 = num + 1; num1 < 9; num1++)
 		{
-			for (num2 = 0; num2 < 10; num2++)
+			for (num2 = num1 + 1; num2 < 10; num2++)
 			{
 				putchar(num % 10 + '0');
 				putchar(num1 % 10 + '0');
