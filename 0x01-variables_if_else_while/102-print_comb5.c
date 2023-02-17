27b9c9dc7222925a9da5b74cabbf6fddcd1a51ee
Biset Desalegn <bisetsamara@gmail.com>
2023-02-17 14:32:32 -0800

    final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,15 @@ int main(void)
 {
 	int num, num1;
 	/* prints all possible combinations of single-digit numbers. */
-	for (num = 0; num < 98; num++)
+	for (num = 0; num <= 98; num++)
 	{
-		for (num1 = num + 1; num1 < 99; num1++)
+		for (num1 = num + 1; num1 <= 99; num1++)
 		{
-			putchar(num + '0');
-			putchar(num + '0');
+			putchar(num / 10 + '0');
+			putchar(num % 10 + '0');
 			putchar(' ');
-			putchar(num1 + '0');
-			putchar(num1 + '0');
+			putchar(num1 / 10 + '0');
+			putchar(num1 % 10 + '0');
 			if (num == 98 && num1 == 99)
 				continue;
 			putchar(',');
