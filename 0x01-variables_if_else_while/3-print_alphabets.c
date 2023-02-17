3f13281828774f26266c34e3b5ca3ae20ef109c0
Biset Desalegn <bisetsamara@gmail.com>
2023-02-17 08:56:05 -0800

    3 Alphabetical order

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char CH;
+
+	/* print lowercase letters */
+	for (CH = 'A' ; CH <= 'z'; CH++)
+	{
+		putchar(CH);
+	}
+	putchar('\n');
+	return (0);
+}
