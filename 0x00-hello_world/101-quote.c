03daf0419cdead860433882333ddc9428c15ec2c
Biset Desalegn <bisetsamara@gmail.com>
2023-02-19 12:46:39 -0800

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	gets(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	gets( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
