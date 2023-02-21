8427218b57f375c08e2eb0c30cae4b7ac11b2b58
Biset Desalegn <bisetsamara@gmail.com>
2023-02-21 12:42:39 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print  aplphabets
+ *
+ * Retuen: always 0.
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
+	_putchar('\n');
+}
