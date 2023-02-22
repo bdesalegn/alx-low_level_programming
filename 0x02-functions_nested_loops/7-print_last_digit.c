2d32192b8fc8e9a9d3ee23e3420b3f7b659ae0ef
Biset Desalegn <bisetsamara@gmail.com>
2023-02-22 07:32:33 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,20 @@
 /**
   * print_last_digit - Computes the absolute value of an integer.
   * @n: The digits.
-  * Return: Always n%10
+  * Return: Always lastDigit.
   */
 int print_last_digit(int n)
 {
 	int lastDigit;
 
 	lastDigit = n % 10;
+
+	if (lastDigit < 0)
+	{
+		lastDigit = lastDigit * -1;
+	}
+
+	_putchar(lastDigit + '0');
+
 	return (lastDigit);
 }
