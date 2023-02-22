5b29b9545479d9d85bad71f85a5902224e6d51e1
Biset Desalegn <bisetsamara@gmail.com>
2023-02-22 12:05:15 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	unsigned long int i, a = 1, b = 2, c;
 
-	printf("%lu ,%lu", a, b);
+	printf("%lu, %lu", a, b);
 	for (i = 3; i <= 50; i++)
 	{
 		c = a + b;
-		printf(" ,%lu", c);
+		printf(", %lu", c);
 		a = b;
 		b = c;
 	}
