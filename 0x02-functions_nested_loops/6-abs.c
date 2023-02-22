6ce5e14987da5fec87c2d605ddd327793ee6d9af
Biset Desalegn <bisetsamara@gmail.com>
2023-02-22 06:12:15 -0800

    absulute value of the integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * _abs - compute the absulute value of the integer.
+ * @ab_num: input integer numbers number
+ * Return: Always x.
+ */
+int _abs(int ab_num)
+{
+	int x;
+
+	x = abs(ab_num);
+	return (x);
+}
+
