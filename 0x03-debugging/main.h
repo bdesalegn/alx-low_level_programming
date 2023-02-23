c99cb9a2532559becd9236cf64fb69db37dba2ff
Biset Desalegn <bisetsamara@gmail.com>
2023-02-23 11:25:04 -0800

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <Stdio.h>
+
 void positive_or_negative(int i);
-void print_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-int largest_number(int day, int year);
 
 #endif
