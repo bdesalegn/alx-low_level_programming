65d60d21acbf7d6984122067e75d7cf072723322
Biset Desalegn <bisetsamara@gmail.com>
2023-02-23 10:45:41 -0800

    debugging

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+what is Debugging 
+What are some methods of debugging manually
+How to read the error messages
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 void positive_or_negative(int i);
-
+void print_days(int month, int day, int year);
+int convert_day(int month, int day);
+int largest_number(int day, int year);
 
 #endif
