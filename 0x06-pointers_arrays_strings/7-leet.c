e18544d2872c48b0d29489886dcc92cd9abb59ba
Biset Desalegn <bisetsamara@gmail.com>
2023-03-02 05:53:46 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,28 +3,27 @@
 /**
  * leet - Write a function that encodes a string into 1337
  *
- * @str:This is th  input string
+ * @str: This is the input string
  *
- * Return: String converted
+ * Return: String converted to 1337
  */
-
 
 char *leet(char *str)
 {
-	char *result = str;
-	char *leetChars[] = {"4", "3", "0", "7", "1"};
-	char *normalChars[] = {"aA", "eE", "oO", "tT", "lL"};
-	/*char *smallChar[] = {"a", "e", "o", "t", "l"};*/
+	int i, j;
+	char smallChars[] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char capChars[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+	char numbers[] = {'4', '3', '0', '7', '1', '\0'};
 
-	for (int i = 0; str[i] != '\0'; ++i)
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		for (int j = 0; j < 5; j++)
+		for (j = 0; j < 5; j++)
 		{
-			if (str[i] == normalChars[j] || str[i] == normalChars[j])
+			if (str[i] == smallChars[j] || str[i] == caChars[j])
 			{
-				result[i] = leetChars[j];
+				str[i] = numbers[j];
 			}
 		}
 	}
-	return (result);
+	return (str);
 }
