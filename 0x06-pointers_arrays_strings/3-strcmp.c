a762676f3c406a46cdf4354a784888401c70aad6
Biset Desalegn <bisetsamara@gmail.com>
2023-03-02 01:30:06 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,26 +14,9 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (1);
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] > s2[i]);
 	}
 
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
-		return (0);
-	}
-	else if (s1[i] == '\0')
-	{
-		return (-1);
-	}
-	else
-	{
-		return (1);
-	}
+	return (0);
 }
