f8cbdcd1a4622b709ae0f587422a7dfb28e7f7b3
Biset Desalegn <bisetsamara@gmail.com>
2023-03-02 03:53:19 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: This is the input string
  * @s2: This is the input string
  *
- * Return: If the strings are equals return "0", if not return other number
+ * Return: If the strings are equals return 0
  */
 
 int _strcmp(char *s1, char *s2)
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] = s2[i]);
+			return (s1[i] - s2[i]);
 	}
 
 	return (0);
