4dad9e895d4e16130e95aea552ac75e6456c77ba
Biset Desalegn <bisetsamara@gmail.com>
2023-03-06 11:39:23 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * _strstr - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  *
+  * Return: char value
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+		while (*haystack != '\0')
+		{
+			char *h = haystack;
+			char *n = needle;
+
+			while (*h == *n && *n != '\0')
+			{
+				h++;
+				n++;
+			}
+			if (*n == '\0')
+			{
+				return (haystack);
+			}
+			haystack++;
+		}
+		return (NULL);
+	}
+}
