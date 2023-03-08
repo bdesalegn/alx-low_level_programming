3ace34bb21f38df6c7258f401ca197384ca2e96c
Biset Desalegn <bisetsamara@gmail.com>
2023-03-08 07:21:20 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,28 +2,14 @@
 /**
  * factorial - gets factorial of n
  * @n: positive integer or 0
- * Return : On sucess 1. 
- * 
+ * Return : On sucess 1.
+ *
  */
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);  /*if  n is negative */
-	}
-	else if (n == 0)
-	{
+	if (n == 0)
 		return (1);  /* because : 0! = 1*/
-	}
-	else
-	{
-		int i, fact = 1;
-
-		for (i = 1; i <= n; ++i)
-		{
-			fact *= i;
-				
-		}
-		return (fact);
-	}
+	return (n * factoial(n - 1));
 }
