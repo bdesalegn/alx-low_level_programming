bff2c6539f429d4fc8aedc84f3f7c153f397e84c
Biset Desalegn <bisetsamara@gmail.com>
2023-03-10 23:56:11 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print name
+ * @argc: count integer value.
+ * @argv: string
+ * Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
