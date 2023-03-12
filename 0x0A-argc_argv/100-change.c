77541404114bcde7c951c6abf60657b4faed5e72
Biset Desalegn <bisetsamara@gmail.com>
2023-03-12 15:21:45 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int change_cents(int num_coins);
+int change_cents(int cents);
 /**
  * main - print minimum numbers of coins
  * @argc: integer value
@@ -14,38 +14,36 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	if (argc < 0)
 	{
-		printf("0\n");
+		/**
+		 * printf("0\n");
+		 */
 		return (0);
 	}
-	printf("%d\n", change_cents(atoi(argv[1])));
+	printf("%d\n",change_cents(atoi(argv[1])));
 	return (0);
 }
 /**
  * change_cents - get change
- * @num_coins: amount of coins from main function
+ * @cents: amount of coins from main function
  * Return: change
  */
 
-int change_cents(int num_coins)
+int change_cents(int cents)
 {
-	int i;
-	int denominations[] = {25, 10, 5, 2, 1};
+	int i, num_coins = 0;
+       	int denominations[] = {25, 10, 5, 2, 1};
+	int num_denominations = 5;
 
-	num_coins = 0;
-
-	for (i = 0; i < denominations[i]; i++)
+	for (i = 0; i < num_denominations; i++)
 	{
-		num_coins += denominations[i]/denominations[i];
+		int denomination = denominations[i];
+		int coins = cents / denomination;
 
-
-		/**
-		 * num_coins -= denomination;
-		 * int coins = cents / denomination;
-		 * num_coins += coins;
-		 * cents -= coins * denomination;
-		 */
+		num_coins += coins;
+		cents -= coins * denomination;
 	}
-	return (num_coins);
+	return num_coins;
 }
