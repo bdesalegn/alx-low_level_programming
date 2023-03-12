50b0dfad3976588d439e35e5b1a079e615d8d323
Biset Desalegn <bisetsamara@gmail.com>
2023-03-12 11:27:55 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add positive numbers
+ * @argc: integer value
+ * @argv: string value
+ * Return: 1 if the argument is two eals error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		int result = num1 +  num2;
+
+		printf("%d\n", result);
+	}
+	return (0);
+}
