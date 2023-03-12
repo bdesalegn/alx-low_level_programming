bddd052474f6cb13bbb486e2717532c1b6b54b9f
Biset Desalegn <bisetsamara@gmail.com>
2023-03-12 12:54:35 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,21 +10,29 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		int num = atoi(argv[i]);
 
-		if (num <= 0)
+			/*if (argv[i][0] == '0')*/
+		if (argv[i][0] < '0' || argv[i][0] > '9')
 		{
-			if (argv[i][0] == '0')
-			{
-				printf("0\n");
-			}
-			else
-			{
-				printf("Error\n");
-			}
+			printf("Error\n");
 			return (1);
+				/**
+				 * printf("0\n");
+				 * }
+				 * else
+				 * {
+				 * printf("Error\n");
+				 * }
+				 */
 		}
 		sum += num;
 	}
