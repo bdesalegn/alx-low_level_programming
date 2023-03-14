d763d2eab07a4ee6cdcc36954c765ac3a2b06cdf
Biset Desalegn <bisetsamara@gmail.com>
2023-03-14 13:17:07 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,13 +9,13 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+	int i, j, **grid;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	int **grid = malloc(height * sizeof(int *));
+	grid = malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 	{
@@ -23,16 +23,16 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-	grid[i] = calloc(width, sizeof(int));
-	if (grid[i] == NULL)
-	{
-		for (j = 0; j < i; j++)
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
 			free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
 		}
-		free(grid);
-		return (NULL);
-	}
 	}
 	return (grid);
 }
