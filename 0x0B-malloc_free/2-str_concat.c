a634728ade6056bb0df086871ea479fd1471a25f
Biset Desalegn <bisetsamara@gmail.com>
2023-03-14 10:12:23 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "Main.h"
+/**
+ * str_concat - coppy s1 to s2
+ * @s1: string s1.
+ * @s2: string s2.
+ * Return: return null
+ */
+char *str_concat(char *s1, char *s2)
+{
+	if (!s1)
+	s1 = "";
+	if (!s2)
+	s2 = "";
+	size_t i, len1 = 0;
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	size_t len2 = 0;
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	char *result = malloc((len1 + len2 + 1) * sizeof(char))
+
+	if (!result)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (i = 0; i < len2; i++)
+	{
+		result[len1 + i] = s2[i];
+	}
+	result[len1 + len2] = '\0';
+	return (result);
+}
