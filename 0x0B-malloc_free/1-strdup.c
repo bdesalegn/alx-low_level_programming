c483d983aee2a17318c70afac898680c941e4763
Biset Desalegn <bisetsamara@gmail.com>
2023-03-14 07:09:39 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,19 +8,20 @@
  */
 char *_strdup(char *str)
 {
+	unsigned int i, len = 0;
+	char *cop;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	unsigned int i, len = 0;
-
+	
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	char *cop;
-
-	cop = malloc(sizeof(char) * len + 1);
+	
+	cop = malloc(sizeof(char) * len);
 	if (cop == NULL)
 	{
 		return (NULL);
