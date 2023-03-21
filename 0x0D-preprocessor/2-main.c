aa10de1aec99efe99f44cb14aad8ac7ad87f8951
Biset Desalegn <bisetsamara@gmail.com>
2023-03-21 10:09:29 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("This program was compiled from the file %s\n", __FILE__);
+	return (0);
+}
