069e1f33beb4f4072272e5d6ff1ec10faa34a1b8
Biset Desalegn <bisetsamara@gmail.com>
2023-03-23 02:05:05 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 /*
- * print_name - print the name of  students.
+ * print_name - print the name of students.
  * @name: pointer char variable
  * @f: pointer to function
- * Return: Always  nothing
+ *
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		exit(1);
 	f(name);
 }
