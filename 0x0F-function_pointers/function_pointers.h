22de4647cb1d5cc4ac8241f11d314c47598e2bcb
Biset Desalegn <bisetsamara@gmail.com>
2023-03-22 23:58:04 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,23 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stddef.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _putchar - write the characther c to stdout
+ * @c: The character to print 
+ *
+ * Return: On Success 1 else appropriate error.
+ */
+/*prototype _putchar*/
 int _putchar(char c);
+
+/*prototype function that prints a name.*/
 void print_name(char *name, void (*f)(char *));
+
+/*prototype function that executes a function*/
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/*prototype  function that searches for an integer.*/
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-#endif
+
+#endif/* FUNCTION_POINTERS */
