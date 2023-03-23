1786c2b9a201a0def14e0f172b4bf2c413f19fe1
Biset Desalegn <bisetsamara@gmail.com>
2023-03-23 03:25:07 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,27 +8,43 @@
  * @argv: vector arguments
  * Return: 0 if false, something else otherwise.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int a, b;
-	int result;
-	int (*op_func)(int, int);
+	int (*get)(int, int);
+	int a, b, operator;
+
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
+	if (strlen(argv[2]) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	operator = argv[2][0];
+	switch (operator)
+	{
+	case '+':
+		break;
+	case '-':
+		break;
+	case '*':
+		break;
+	case '/':
+		break;
+	case '%':
+		break;
+	default:
+		printf("Error\n");
+		exit(99);
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
-
-	if (op_func == NULL)
-	{
-		printf("Erro\n");
-		return (99);
-	}
-	result = op_func(a, b);
-	printf("%d\n", result);
+	get = get_op_func(argv[2]);
+	printf("%d\n", get(a, b));
 	return (0);
 }
