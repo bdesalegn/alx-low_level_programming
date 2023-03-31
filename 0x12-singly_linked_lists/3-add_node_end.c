09a48690d410e44c2fd469b140ec07a06d2dde38
Biset Desalegn <bisetsamara@gmail.com>
2023-03-31 06:53:04 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,44 +1,39 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-/**
-  * add_node_end - add new nodes to the end of the list
-  * @head: current place in the list
-  * @str: string to add to the head
-  * Return: pointer to current position in list
-  */
+#include "lists.h"
 
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	char *str_copy;
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	if (new_node == NULL)
-	{
-		return (NULL); /* Failed to allocate memory for new node*/
-	}
-	str_copy = strdup(str);
+	while (str[len])
+	len++;
 
-	if (str_copy == NULL)
-	{
-		free(new_node);
-		return (NULL); /* Failed to allocate memory for string copy*/
-	}
-	new_node->str = str_copy;
-	new_node->next = NULL;
-	if (*head == NULL)/* The list is empty*/
-	{
-		*head = new_node;
-	}
-	else
-	{
-		list_t *last_node = *head;
+	new = malloc(sizeof(list_t));
+	if (!new)
+	return (NULL);
 
-		while (last_node->next != NULL)
-		{
-			last_node = last_node->next;
-		}
-		last_node->next = new_node;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
 	}
-	return (new_node);
+
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
 }
