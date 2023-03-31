3c6ff0bc13f07c888fe9c10b3aa0ad58d39a3018
Biset Desalegn <bisetsamara@gmail.com>
2023-03-31 07:03:11 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_intro - prints a sentence before the main
+ * function is executed
+ */
+
+void print_intro(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
