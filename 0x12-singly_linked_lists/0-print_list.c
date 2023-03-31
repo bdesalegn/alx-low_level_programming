ed3e895a652a1436188e6433992417143bd32ff6
Biset Desalegn <bisetsamara@gmail.com>
2023-03-31 07:20:23 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (current->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", current->len, current->str);
+			printf("[%u] %s\n", current->len, current->str);
 		count++;
 	}
 	return (count);
