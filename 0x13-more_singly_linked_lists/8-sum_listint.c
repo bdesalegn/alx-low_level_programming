d9af766b57db552ee16cbcb9600fecf12d9716e2
Biset Desalegn <bisetsamara@gmail.com>
2023-04-03 13:16:40 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_listint - sums up all the data in a linked list
+ * @head: head of the list
+ *
+ * Return: sum of the number
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0; /* variable to store the sum of data */
+
+    /* iterate over the linked list */
+	while (head != NULL)
+	{
+		sum += head->n; /* add the data of the current node to the sum */
+		head = head->next; /* move to the next node */
+	}
+	return (sum);
+}
