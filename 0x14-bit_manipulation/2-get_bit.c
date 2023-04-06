9994aab3ba16c44631de800eee5add430675fdac
Biset Desalegn <bisetsamara@gmail.com>
2023-04-06 14:56:58 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,7 +17,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	}
 
-	int bit = (n >> index) & 1;
+	/*int bit = (n >> index) & 1;*/
 
-	return (bit);
+	return ((n >> index) & 1);
 }
