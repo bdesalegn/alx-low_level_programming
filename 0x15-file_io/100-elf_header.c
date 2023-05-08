5248656a4dbcb4e41ce2179ce93ba83873c8012d
bdesalegn <bisetsamara@gmail.com>
2023-05-08 11:44:15 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,57 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <elf.h>
 
+/**
+ * obtain_file_type - determine the type of files
+ * @ehdr: pointer to struct of ELF header features
+ */
+voide obtain_file_type(Elf64_Ehdr *ehdr)
+{
+	printf("%-35s", "Type:");
+	switch (ehdr->e_type)
+	{
+	case ET_NONE:
+		printf("NONE (Unknown type)\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("CORE (Core file)\n");
+		break;
+	}
+}
+
+
+/**
+ * main - Begining point for program to get header of ELF file
+ * @argc: arguments  of count
+ * @argv: arguments of array
+ *
+ * Return: 1 on success, error code on failure
+ */
 int main(int argc, char *argv[])
 {
-	int i, fd = open(argv[1], O_RDONLY);
-	Elf64_Ehdr elf_header;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-		exit(98);
-	}
-	/*int i, fd = open(argv[1], O_RDONLY);*/
-
-	if (fd < 0)
-	{
-		fprintf(stderr, "Error: unable to open file '%s'\n", argv[1]);
-		exit(98);
-	}
-
-	/*Elf64_Ehdr elf_header;*/
-
-	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-	{
-		fprintf(stderr, "Error: unable to read ELF header from file '%s'\n", argv[1]);
-		exit(98);
-	}
-	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-			elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-			elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-			elf_header.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		fprintf(stderr, "Error: '%s' is not an ELF file\n", argv[1]);
-		exit(98);
-	}
-
-	printf("Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", elf_header.e_ident[i]);
-	}
-	printf("\n");
-
-	printf("Class:                             %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("Data:                              %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2MSB ? "big-endian" : "little-endian");
-	printf("Version:                           %d\n", elf_header.e_ident[EI_VERSION]);
-	printf("OS/ABI:                            %d\n", elf_header.e_ident[EI_OSABI]);
-	printf("ABI Version:                       %d\n", elf_header.e_ident[EI_ABIVERSION]);
-	printf("Type:                              %d\n", elf_header.e_type);
-	printf("Entry point address:               0x%lx\n", elf_header.e_entry);
-
-	return (0);
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
 }
