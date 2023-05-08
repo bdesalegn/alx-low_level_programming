575fa44d3aaeeccdc441c6afb778c078e0702284
bdesalegn <bisetsamara@gmail.com>
2023-05-08 13:23:10 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,15 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void check_e(unsigned char *elf_array);
+void print_magic(unsigned char *elf_array);
+void print_class(unsigned char *elf_array);
+void print_data(unsigned char *elf_array);
+void print_version(unsigned char *elf_array);
+void print_abi(unsigned char *elf_array);
+void print_osabi(unsigned char *elf_array);
+void print_type(unsigned int elf_type, unsigned char *elf_array);
+void print_entry(unsigned long int elf_entry, unsigned char *elf_array);
+void close_elf(int elf);
 
 #endif
