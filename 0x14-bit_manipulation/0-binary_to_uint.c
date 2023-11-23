a49d91dcd708c8c4a5fbe3bd617ea2d5f8e5a17f
Biset Desalegn <bisetsamara@gmail.com>
2023-11-23 04:03:41 -0800

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,21 @@
  * Return: the converted binary number,or 0
  *
  */
-unsigned int binary_to_uint(const char *a)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int res = 0;
 
-	if (a == NULL)
+	if (b == NULL)
 	return (0);
 
 	/* repeate each character of the string */
-	for (; *a != '\0'; a++)
+	for (; *b != '\0'; b++)
 	{
 		/* Check if the character is not 0 or 1 */
-		if (*a != '0' && *a != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
-	result <<= 1;
-	result += (*a - '0');
+	res <<= 1;
+	res += (*b - '0');
 	}
-	return (result);
+	return (res);
 }
