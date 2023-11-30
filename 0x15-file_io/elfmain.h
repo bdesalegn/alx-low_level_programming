8048f67b9aa5614aedecf839f02d6a1fffd319bb
Biset Desalegn <bisetsamara@gmail.com>
2023-11-29 23:34:30 -0800

    elfmain.h

--- 0x15-file_io/elfmain.h
+++ 0x15-file_io/elfmain.h
@@ -1,17 +1,16 @@
 #ifndef ELFMAIN_H
-#define ELFMAIN_H
+#define ELFMAIN_Hi
 
-/*Function Prototypes */
-void check_elf(unsigned char *ident);
-void display_magic(unsigned char *ident);
-void display_class(unsigned char *ident);
-void display_data(unsigned char *ident);
-void display_version(unsigned char *ident);
-void display_abi(unsigned char *ident);
-void display_osabi(unsigned char *ident);
-void display_type(unsigned int e_type, unsigned char *ident);
-void display_entry(unsigned long int e_entry, unsigned char *ident);
+/* Function Prototypes */
+void check_elf(unsigned char *e_ident);
+void display_magic(unsigned char *e_ident);
+void display_class(unsigned char *e_ident);
+void display_data(unsigned char *e_ident);
+void display_version(unsigned char *e_ident);
+void display_abi(unsigned char *e_ident);
+void display_osabi(unsigned char *e_ident);
+void display_type(unsigned int e_typee, unsigned char *e_ident);
+void display_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 #endif
-
