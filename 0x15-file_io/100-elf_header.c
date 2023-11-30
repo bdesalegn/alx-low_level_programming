60af1a49f9dca2ec5b6d6e57bfc9174d5dac99fe
Biset Desalegn <bisetsamara@gmail.com>
2023-11-30 00:17:11 -0800

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,8 +1,17 @@
 #include <fcntl.h>
-#include <elf.h>
-#include "elfmain.h"
+#include <elf>
 #include "main.h"
-
+/* functional prototype*/
+void check_elf(unsigned char *e_ident);
+void display_magic(unsigned char *e_ident);
+void display_class(unsigned char *e_ident);
+void display_data(unsigned char *e_ident);
+void display_version(unsigned char *e_ident);
+void display_abi(unsigned char *e_ident);
+void display_osabi(unsigned char *e_ident);
+void display_type(unsigned int e_typee, unsigned char *e_ident);
+void display_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
